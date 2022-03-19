b84b071a9a8a401fcc92bd2e8b8694d0f3d29c8e
Pedro Queiroz <pccql@cin.ufpe.br>
2022-03-19 12:42:15 -0300

    feat(master): main function forEach switch

--- index.ts
+++ index.ts
@@ -5,26 +5,29 @@ TRAILS.forEach((trail) => {
   let actualState = 0;
 
   console.log(`${TRAILS.indexOf(trail) + 1}º iteraction`);
-  trail.forEach((currAction, nextAction) => {
-    const actionPath = randomAction(currAction[0]);
-    const nextStatePath = nextState(actualState, actionPath[0]);
+
+  for (const [currAction, nextAction] of trail) {
+    const actionPath = randomAction(currAction);
+    const nextStatePath = nextState(actualState, nextAction);
 
     console.log(`${actualState + 1} | ${currAction} x ${nextAction} -> ${nextStatePath + 1}`);
 
-    QMATRIX[actualState][actionPath[0]] = update(
-      actualState, actionPath[0], nextStatePath,
-      REWARDS, QMATRIX, ALPHA, GAMMA
+    QMATRIX[actualState][actionPath] = update(
+      actualState, actionPath, nextStatePath, REWARDS, QMATRIX, ALPHA, GAMMA
     );
 
     actualState = nextStatePath;
 
-    if (nextAction === trail.length - 1) break;
+    if (actualState === trail.length - 1) break;
+
     console.log(getBestPolicy(QMATRIX));
-    QMATRIX.map((lists) => {
-      lists.map((numbers) => {
+
+    QMATRIX.forEach((lists) => {
+      lists.forEach((numbers) => {
         QMATRIX[QMATRIX.indexOf(lists)][lists.indexOf(numbers)] = Number(numbers.toFixed(2));
       });
     });
+
     console.log(QMATRIX);
-  });
+  }
 });
